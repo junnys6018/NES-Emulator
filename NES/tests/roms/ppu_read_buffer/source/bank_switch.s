718bf1c16783adef45a50492ecadbf44ea7a6c5d
junnys6018 <junkanglim1@gmail.com>
2020-10-11 21:10:52 +0800

    Stopped Tracking tests

--- NES/tests/roms/ppu_read_buffer/source/bank_switch.s
+++
@@ -1,6 +0,0 @@
-.macro set_vrom_page n
-	lda #(n) ;VROM page n
-	sta bus_conflict_antidote+(n)
-.endmacro
-
-seg_data "RODATA", { bus_conflict_antidote: .byte 0,1,2,3 }
--- NES/tests/roms/ppu_read_buffer/source/testing2-end.s
+++
@@ -1,2 +0,0 @@
-seg_data "POINTERS", { NUM_TESTS = (*-TestNames)/2 + 1 }
-seg_data "TEST_RESULTS", { .res NUM_TESTS+1 }
