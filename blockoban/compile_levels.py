e6b8b99b1edf3a09ba5a7da22f09ad6fa20338d0
junnys6018 <junkanglim1@gmail.com>
2020-12-18 16:33:54 +0800

    Clean up level compilation and documented ram layout during runtime

--- blockoban/compile_levels.py
+++ blockoban/compile_levels.py
@@ -1,42 +1,30 @@
-foreground = {'0': 0, '#': 1, 'B': 2, 'F': 3}
-background = {'0': 0, 'B': 1}
+tilemap = {'0': 0, 'B': 1, '#': 1 << 4, 'C': 2 << 4, 'F': 3 << 4}
 
-content = open('levels.txt').read().splitlines()
+levels = open('levels.txt').read().splitlines()
 
 # remove empty lines
-content = [line for line in content if line.strip()]
+levels = [line for line in levels if line.strip()]
 
 # concatinate blocks of 15 lines into one string
-content = [''.join(content[i: i + 15]) for i in range(0, len(content), 15)]
-
-# even elements define the foreground of a level
-fg_elems = content[::2]
-
-# odd elements define the background of a level
-bg_elems = content[1::2]
+levels = [''.join(levels[i: i + 15]) for i in range(0, len(levels), 15)]
 
 # serialize and write to file
 output = bytearray()
-for (fg_elem, bg_elem) in zip(fg_elems, bg_elems):
-	num_buttons = 0
-	flag_pos = 0
-	flag_found = False
-	for (fg, bg) in zip(fg_elem, bg_elem):
-		output.append(foreground[fg] << 4 | background[bg])
+for level in levels:
+    num_buttons = 0
+    flag_pos = 0
+    for index, tile in enumerate(level):
+        output.append(tilemap[tile])
 
-		if (bg == 'B'):
-			num_buttons += 1
-		if (fg == 'F'):
-			flag_found = True
-		if (not flag_found):
-			flag_pos += 1
-		if (bg == 'B' and (fg != '0')):
-			print("[WARN] Button is below non air tile")
+        if (tile == 'B'):
+            num_buttons += 1
+        elif (tile == 'F'):
+            flag_pos = index
 
-	footer = bytearray([0] * 16)
-	footer[0] = flag_pos
-	footer[1] = num_buttons
-	
-	output = output + footer
+    footer = bytearray([0] * 16)
+    footer[0] = flag_pos
+    footer[1] = num_buttons
+
+    output = output + footer
 
 open('levels.bin', 'wb').write(output)
