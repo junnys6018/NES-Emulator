e36e6faee92ba97c2e7a3e11bdd5e8504b32cfd9
Jun <junkanglim1@gmail.com>
2021-04-06 14:24:43 +0800

    Implemented textured quads in batch renderer

--- src/Frontend/OpenGL/BatchRenderer.c
+++ src/Frontend/OpenGL/BatchRenderer.c
@@ -13,14 +13,17 @@ typedef struct
 	float x, y;
 	float r, g, b;
 	float tx, ty;
+	float texture_slot;
 } Vertex;
 
-static GLuint vbo, ibo, vao, shader;
+static GLuint vbo, ibo, vao, shader, white_texture;
 static GLint u_transform;
 static bool initialized = false;
 
 static Vertex* buffer_ptr;
 static unsigned int buffer_offset;
+static GLuint active_textures[31];
+static int active_texture_index;
 
 void InitBatchRenderer()
 {
@@ -36,12 +39,14 @@ void InitBatchRenderer()
 		glBufferData(GL_ARRAY_BUFFER, QUADS_PER_BATCH * sizeof(Vertex) * 4, NULL, GL_DYNAMIC_DRAW);
 
 		glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, sizeof(Vertex), (void*)0);
-		glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex), (void*)(2*sizeof(float)));
-		glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, sizeof(Vertex), (void*)(5*sizeof(float)));
+		glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex), (void*)(2 * sizeof(float)));
+		glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, sizeof(Vertex), (void*)(5 * sizeof(float)));
+		glVertexAttribPointer(3, 1, GL_FLOAT, GL_FALSE, sizeof(Vertex), (void*)(7 * sizeof(float)));
 
 		glEnableVertexAttribArray(0);
 		glEnableVertexAttribArray(1);
 		glEnableVertexAttribArray(2);
+		glEnableVertexAttribArray(3);
 
 		glGenBuffers(1, &ibo);
 		glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ibo);
@@ -67,28 +72,53 @@ void InitBatchRenderer()
 			"layout(location = 0) in vec2 a_Position;\n"
 			"layout(location = 1) in vec3 a_Color;\n"
 			"layout(location = 2) in vec2 a_TexCoord;\n"
-			"uniform mat4 u_transform;"
+			"layout(location = 3) in float a_TexIndex;\n"
+			"uniform mat4 u_transform;\n"
 			"out vec3 v_Color;\n"
 			"out vec2 v_TexCoord;\n"
+			"out float v_TexIndex;\n"
 			"void main() {\n"
 			"    gl_Position = u_transform * vec4(a_Position, 0.0, 1.0);\n"
 			"    v_Color = a_Color;\n"
 			"    v_TexCoord = a_TexCoord;\n"
+			"    v_TexIndex = a_TexIndex;\n"
 			"}\n";
 
 		static const char* fragment_src =
 			"#version 330 core\n"
 			"in vec3 v_Color;\n"
 			"in vec2 v_TexCoord;\n"
-			"uniform sampler2D atlas;\n"
+			"in float v_TexIndex;\n"
+			"uniform sampler2D textures[32];\n"
 			"out vec4 color;\n"
 			"void main() {\n"
-			"    color = vec4(v_Color, 1.0);\n"
+			"	int index = int(v_TexIndex);"
+			"   color = vec4(v_Color, 1.0) * texture(textures[index], v_TexCoord);\n"
 			"}\n";
 
 		shader = BuildShader(vertex_src, fragment_src);
 
 		u_transform = glGetUniformLocation(shader, "u_transform");
+		GLint texture_slots[32];
+		for (int i = 0; i < 32; i++)
+		{
+			texture_slots[i] = i;
+		}
+		glUseProgram(shader);
+		glUniform1iv(glGetUniformLocation(shader, "textures"), 32, texture_slots);
+
+		// Create a white 1x1 texture
+		glGenTextures(1, &white_texture);
+		glBindTexture(GL_TEXTURE_2D, white_texture);
+
+		uint32_t data = 0xFFFFFFFF;
+		glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, 1, 1, 0, GL_RGB, GL_UNSIGNED_BYTE, &data);
+
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+
 	}
 	else
 	{
@@ -101,16 +131,20 @@ void ShutdownBatchRenderer()
 	glDeleteBuffers(1, &vbo);
 	glDeleteBuffers(1, &ibo);
 	glDeleteVertexArrays(1, &vao);
+	glDeleteTextures(1, &white_texture);
 	DestroyShader(shader);
 }
 
 void BeginBatch()
 {
+	glBindTextureUnit(0, white_texture);
 	glBindVertexArray(vao);
 	glUseProgram(shader);
 
 	buffer_ptr = glMapNamedBuffer(vbo, GL_READ_WRITE);
+
 	buffer_offset = 0;
+	active_texture_index = 0;
 
 	// Set the transform matrix, transforms SDL screen coordinates into openGL NDC
 	int w, h;
@@ -131,24 +165,19 @@ void EndBatch()
 {
 	glUnmapNamedBuffer(vbo);
 	glDrawElements(GL_TRIANGLES, buffer_offset * 6 / 4, GL_UNSIGNED_INT, 0);
+
+	buffer_offset = 0;
+	active_texture_index = 0;
 }
 
 void flush()
 {
-	glUnmapNamedBuffer(vbo);
-	glDrawElements(GL_TRIANGLES, buffer_offset * 6 / 4, GL_UNSIGNED_INT, 0);
+	EndBatch();
 	buffer_ptr = glMapNamedBuffer(vbo, GL_READ_WRITE);
-	buffer_offset = 0;
-
-	printf("fushed");
 }
 
 void SubmitColoredQuad(int x, int y, int w, int h, int r, int g, int b)
 {
-	if (buffer_offset == QUADS_PER_BATCH * 4)
-	{
-		flush();
-	}
 	Vertex* v = buffer_ptr + buffer_offset;
 	float _r = (float)r / 255.0f;
 	float _g = (float)g / 255.0f;
@@ -160,6 +189,7 @@ void SubmitColoredQuad(int x, int y, int w, int h, int r, int g, int b)
 	v->r = _r;
 	v->g = _g;
 	v->b = _b;
+	v->texture_slot = 0.0f;
 	v++;
 
 	v->x = x + w;
@@ -168,6 +198,7 @@ void SubmitColoredQuad(int x, int y, int w, int h, int r, int g, int b)
 	v->r = _r;
 	v->g = _g;
 	v->b = _b;
+	v->texture_slot = 0.0f;
 	v++;
 
 	v->x = x + w;
@@ -176,6 +207,7 @@ void SubmitColoredQuad(int x, int y, int w, int h, int r, int g, int b)
 	v->r = _r;
 	v->g = _g;
 	v->b = _b;
+	v->texture_slot = 0.0f;
 	v++;
 
 	v->x = x;
@@ -184,11 +216,90 @@ void SubmitColoredQuad(int x, int y, int w, int h, int r, int g, int b)
 	v->r = _r;
 	v->g = _g;
 	v->b = _b;
+	v->texture_slot = 0.0f;
 	v++;
 
 	buffer_offset += 4;
+	if (buffer_offset == QUADS_PER_BATCH * 4)
+	{
+		flush();
+	}
 }
 
-void SubmitTexturedQuad(int x, int y, int w, int h, int atlas_index)
+void SubmitTexturedQuad(int x, int y, int w, int h, GLuint texture, float tx, float ty, float tw, float th)
 {
+	bool texture_already_bound = false;
+	int bound_index = 0;
+	for (; bound_index < active_texture_index; bound_index++)
+	{
+		if (active_textures[bound_index] == texture)
+		{
+			texture_already_bound = true;
+			break;
+		}
+	}
+
+	if (!texture_already_bound)
+	{
+		bound_index = active_texture_index++;
+		active_textures[bound_index] = texture;
+		glBindTextureUnit(bound_index + 1, texture);
+	}
+
+	if (tw == 0.0f || th == 0.0f)
+	{
+		tx = 0.0f;
+		ty = 0.0f;
+		tw = 1.0f;
+		th = 1.0f;
+	}
+
+	Vertex* v = buffer_ptr + buffer_offset;
+	float slot = bound_index + 1;
+
+	v->x = x;
+	v->y = y;
+	v->tx = tx;
+	v->ty = ty;
+	v->r = 1.0f;
+	v->g = 1.0f;
+	v->b = 1.0f;
+	v->texture_slot = slot;
+	v++;
+
+	v->x = x + w;
+	v->y = y;
+	v->tx = tx + tw;
+	v->ty = ty;
+	v->r = 1.0f;
+	v->g = 1.0f;
+	v->b = 1.0f;
+	v->texture_slot = slot;
+	v++;
+
+	v->x = x + w;
+	v->y = y + h;
+	v->tx = tx + tw;
+	v->ty = ty + th;
+	v->r = 1.0f;
+	v->g = 1.0f;
+	v->b = 1.0f;
+	v->texture_slot = slot;
+	v++;
+
+	v->x = x;
+	v->y = y + h;
+	v->tx = tx;
+	v->ty = ty + th;
+	v->r = 1.0f;
+	v->g = 1.0f;
+	v->b = 1.0f;
+	v->texture_slot = slot;
+	v++;
+
+	buffer_offset += 4;
+	if (buffer_offset == QUADS_PER_BATCH * 4 || active_texture_index == 31)
+	{
+		flush();
+	}
 }
--- src/Frontend/OpenGL/BatchRenderer.h
+++ src/Frontend/OpenGL/BatchRenderer.h
@@ -1,5 +1,6 @@
 #ifndef BATCH_RENDERER_H
 #define BATCH_RENDERER_H
+#include <glad/glad.h>
 
 void InitBatchRenderer();
 void ShutdownBatchRenderer();
@@ -7,6 +8,6 @@ void ShutdownBatchRenderer();
 void BeginBatch();
 void EndBatch();
 void SubmitColoredQuad(int x, int y, int w, int h, int r, int g, int b);
-void SubmitTexturedQuad(int x, int y, int w, int h, int atlas_index);
+void SubmitTexturedQuad(int x, int y, int w, int h, GLuint texture, float tx, float ty, float tw, float th);
 
 #endif
