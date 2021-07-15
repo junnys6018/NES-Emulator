d9d96e04f920074dfe8bbae00d032c589b18d8f6
junnys6018 <junkanglim1@gmail.com>
2021-07-15 23:09:07 +0800

    Update project generation scripts

--- scripts/GenerateLinuxProjects.sh
+++ scripts/GenerateLinuxProjects.sh
@@ -1,3 +1,3 @@
-pushd ../
+pushd "$(dirname "$0")/.."
 vendor/premake5/premake5_linux --os=linux gmake2
 popd
--- scripts/GenerateWindowsProjects.bat
+++ scripts/GenerateWindowsProjects.bat
@@ -1,4 +1,3 @@
-pushd ..\
+pushd %~dp0..
 call vendor\premake5\premake5_windows.exe --os=windows vs2019
 popd
-PAUSE
